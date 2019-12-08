4dbc5a32dc959905ad5d8d13a6eb9d1bcbc0c09a
Rob Nelson <nexisentertainment@gmail.com>
2019-12-07 21:51:40 -0800

    Fix compile process.

---
+++ TODO.md
@@ -0,0 +1,2 @@
+# TODO
+* Add notes database
--- build.py
+++ build.py
@@ -3,10 +3,12 @@ import sys
 
 from buildtools import log, os_utils
 from buildtools.maestro import BuildMaestro
+from buildtools.maestro.git import GitSubmoduleCheckTarget
 from buildtools.maestro.base_target import SingleBuildTarget
 from buildtools.maestro.fileio import CopyFileTarget, CopyFilesTarget
 from buildtools.maestro.coffeescript import CoffeeBuildTarget
-from buildtools.maestro.nodejs import YarnBuildTarget, ComposerBuildTarget
+from buildtools.maestro.package_managers import YarnBuildTarget, ComposerBuildTarget
+from buildtools.maestro.web import CacheBashifyFiles, DartSCSSBuildTarget
 
 class JQueryUIBuildTarget(SingleBuildTarget):
     BT_LABEL = 'JQUERYUI'
@@ -20,18 +22,23 @@ class JQueryUIBuildTarget(SingleBuildTarget):
 
     def build(self):
         with os_utils.Chdir(os.path.join(self.base_path)):
-            os_utils.cmd([os_utils.which('npm'), 'install'], echo=True, show_output=True, critical=True)
+            os_utils.cmd([os_utils.which('yarn')], echo=True, show_output=True, critical=True)
             os_utils.cmd([os_utils.which('grunt'), 'requirejs', 'uglify:main'], echo=True, show_output=True, critical=True)
 
+os_utils.ENV.prependTo('PATH', os.path.join('node_modules','.bin'))
 bm = BuildMaestro()
 
+YARNLIB = os.path.join('node_modules')
 HTDOCS_JSLIB = os.path.join('htdocs','js','lib')
 HTDOCS_CSSLIB = os.path.join('htdocs','css', 'lib')
 BOOTSTRAP_ROOT = os.path.join('vendor', 'twbs', 'bootstrap', 'dist', 'fonts')
 FONTS = ['glyphicons-halflings-regular']
 FONT_EXT = ['eot', 'svg', 'ttf', 'woff', 'woff2']
+COFFEE = os.path.join(YARNLIB, '.bin', 'coffee')
 
-yarn_install = YarnBuildTarget(modules_dir=os.path.join('lib','js'))
+submodules = bm.add(GitSubmoduleCheckTarget())
+
+yarn_install = YarnBuildTarget()
 bm.add(yarn_install)
 
 composer_install = ComposerBuildTarget()
@@ -42,29 +49,49 @@ for font_id in FONTS:
         basefilename = font_id + '.' + ext
         bm.add(CopyFileTarget(os.path.join('htdocs', 'fonts', basefilename), os.path.join(BOOTSTRAP_ROOT,basefilename), verbose=False))
 
-JQUERY_ROOT = os.path.join('lib','js','jquery')
+JQUERY_ROOT = os.path.join(YARNLIB,'jquery')
 bm.add(CopyFileTarget(os.path.join(HTDOCS_JSLIB, 'jquery.min.js'), os.path.join(JQUERY_ROOT, 'dist', 'jquery.min.js'), dependencies=[yarn_install.target]))
 bm.add(CopyFileTarget(os.path.join(HTDOCS_JSLIB, 'jquery.min.map'), os.path.join(JQUERY_ROOT, 'dist', 'jquery.min.map'), dependencies=[yarn_install.target]))
 
-JQUERY_UI_ROOT = os.path.join('lib','js','jquery-ui')
-jqui_builder = JQueryUIBuildTarget(JQUERY_UI_ROOT)
-bm.add(jqui_builder)
-bm.add(CopyFileTarget(os.path.join(HTDOCS_JSLIB, 'jquery-ui.min.js'), jqui_builder.target, dependencies=[jqui_builder.target, yarn_install.target]))
+TAGIT_ROOT = os.path.join('lib','tag-it.js')
+# Tag-It
+bm.add(CopyFileTarget(
+    filename=os.path.join(TAGIT_ROOT, 'js', 'tag-it.min.js'),
+    target=os.path.join(HTDOCS_JSLIB, 'tag-it.min.js')
+))
 
-TAGIT_ROOT = os.path.join('lib','js','tag-it')
-bm.add(CopyFileTarget(os.path.join(HTDOCS_JSLIB, 'tag-it.min.js'), os.path.join(TAGIT_ROOT, 'js', 'tag-it.min.js'), dependencies=[yarn_install.target]))
-#bm.add(CopyFileTarget(os.path.join(HTDOCS_JSLIB, 'jquery.min.map'), os.path.join(TAGIT_ROOT, 'dist', 'tagit.min.map'))) # I wish.
+'''
+JQUERYUI_THEME = os.path.join(JSLIB, 'jquery-ui-themes', 'themes', 'base')
+JQUERYUI_BASE_THEME = os.path.join(JSLIB, 'jquery-ui-themes', 'themes', 'base')
+theme_vared = bm.add(ReplaceTextTarget('tmp/theme.varified.css', os.path.join(JSLIB, 'jquery-ui', 'themes', 'base', 'theme.css'), {
+    r'([\s]+[\S]+| )\/\*\{([^\}\*\/]+)\}\*\/': ' --var-jq_ui_\\2'
+}))
+theme_datafied = bm.add(DatafyImagesTarget('tmp/theme.fixed.css',     theme_vared.target,     basedir=JQUERYUI_THEME))
+theme_converted = bm.add(SCSSConvertTarget(os.path.join('tmp', '_theme2__gen.scss'), [theme_datafied.target], [theme_datafied.target]))
+theme_var2 = bm.add(ReplaceTextTarget('tmp/tag-it.fixed.css', theme_converted.target, {
+    r'\-\-var\-': '$'
+}))
+'''
 
-JQUI_THEME_ROOT = os.path.join('lib','js','jquery-ui-themes', 'themes', 'smoothness')
+JQUI_THEME_ROOT = os.path.join(YARNLIB,'jquery-ui-themes', 'themes', 'smoothness')
 bm.add(CopyFileTarget(os.path.join(HTDOCS_CSSLIB, 'jquery-ui.min.css'), os.path.join(JQUI_THEME_ROOT, 'jquery-ui.min.css'), dependencies=[yarn_install.target]))
 bm.add(CopyFilesTarget(os.path.join(bm.builddir, '.jqui_theme.target'), os.path.join(JQUI_THEME_ROOT, 'images'), os.path.join(HTDOCS_CSSLIB, 'images'), dependencies=[yarn_install.target]))
 
 # cp -rv vendor/twbs/bootstrap-sass/assets/stylesheets/bootstrap/* style/bootstrap/
 source = os.path.join('vendor','twbs','bootstrap-sass', 'assets', 'stylesheets', 'bootstrap')
-bm.add(CopyFilesTarget(os.path.join(bm.builddir, '.bootstrap-sass.target'), os.path.join('style', 'bootstrap'), source, dependencies=[composer_install.target]))
+bootstrap_files = bm.add(CopyFilesTarget(os.path.join(bm.builddir, '.bootstrap-sass.target'), source, os.path.join('style', 'bootstrap'), dependencies=[composer_install.target]))
 
 #bm.add(CoffeeBuildTarget('htdocs/js/vgws.js',    ['coffee/src/vgws.coffee']))
-bm.add(CoffeeBuildTarget(os.path.join('htdocs', 'js', 'editpoll.multichoice.js'), [os.path.join('coffee', 'editpoll.multichoice.coffee')]))
-bm.add(CoffeeBuildTarget(os.path.join('htdocs', 'js', 'editpoll.option.js'),      [os.path.join('coffee', 'editpoll.multichoice.coffee')]))
-#bm.add(SCSSBuildTarget('htdocs/css/style.css', ['style/style.scss'], [], import_paths=['style'], compass=True))
+bm.add(CoffeeBuildTarget(os.path.join('htdocs', 'js', 'editpoll.multichoice.js'), [os.path.join('coffee', 'editpoll.multichoice.coffee')], coffee_executable=COFFEE))
+bm.add(CoffeeBuildTarget(os.path.join('htdocs', 'js', 'editpoll.option.js'),      [os.path.join('coffee', 'editpoll.multichoice.coffee')], coffee_executable=COFFEE))
+bm.add(DartSCSSBuildTarget(
+    target='htdocs/css/style.css',
+    files=['style/style.scss'],
+    dependencies=[
+        yarn_install.target,
+        bootstrap_files.target
+    ],
+    import_paths=['style'],
+    imported=['style/_funcs.scss'],
+    sass_path=os.path.join(YARNLIB, '.bin', 'dart-sass' + ('.cmd' if os_utils.is_windows() else ''))))
 bm.as_app()
